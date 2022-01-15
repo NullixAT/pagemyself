641c4dd9cd06b0a1165ba5c95fa4e4e52a1dc633
BrainFooLong <brainfoolong@users.noreply.github.com>
2022-01-15 14:36:20 +0100

    :pencil2: updates for docker

--- pagemyself_docker/app/index.php
+++ pagemyself_docker/app/index.php
@@ -1,4 +1,33 @@
 <?php
-const FRAMELIX_MODULE = "Myself";
-define("FRAMELIX_ENTRY_POINT_FOLDER", str_replace("\\", "/", __DIR__));
-require __DIR__ . "/modules/Framelix/public/index.php";
+
+// download latest release, unpack and link to install.php
+$context = stream_context_create([
+        'http' => [
+            'method' => "GET",
+            'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+        ]
+    ]
+);
+$releaseData = json_decode(
+    file_get_contents(
+        'https://api.github.com/repos/NullixAT/pagemyself/releases/latest',
+        false,
+        $context
+    ),
+    true
+);
+$zipFile = __DIR__ . "/release.zip";
+file_put_contents($zipFile, file_get_contents($releaseData['assets'][0]['browser_download_url'], 0, $context));
+
+$zipArchive = new ZipArchive();
+$openResult = $zipArchive->open($zipFile, ZipArchive::RDONLY);
+if ($openResult !== true) {
+    throw new Exception("Cannot open ZIP File '$zipFile' ($openResult)");
+}
+$zipArchive->extractTo(__DIR__);
+$zipArchive->close();
+if (file_exists(__DIR__ . "/install.php")) {
+    unlink($zipFile);
+    unlink(__FILE__);
+    header("location: install.php");
+}
