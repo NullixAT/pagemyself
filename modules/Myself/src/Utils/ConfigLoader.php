48bb6511a616a04ff762bb25bfe84ecaec52a130
BrainFooLong <brainfoolong@users.noreply.github.com>
2022-01-16 17:57:43 +0100

    :pencil2: added enabled modules check to prevent errors if module gets incompatible

--- modules/Myself/src/Utils/ConfigLoader.php
+++ modules/Myself/src/Utils/ConfigLoader.php
@@ -2,7 +2,15 @@
 
 namespace Framelix\Myself\Utils;
 
+use Framelix\Framelix\Utils\JsonUtils;
 use Framelix\Myself\Storable\WebsiteSettings;
+
+use function explode;
+use function file_exists;
+
+use function var_dump;
+
+use const FRAMELIX_APP_ROOT;
 
 /**
  * ConfigLoader
@@ -10,11 +18,39 @@ use Framelix\Myself\Storable\WebsiteSettings;
 class ConfigLoader
 {
     /**
+     * Cache
+     * @var array
+     */
+    private static array $cache = [];
+
+    /**
      * Get additional enabled modules
      * @return string[]
      */
     public static function getModules(): array
     {
-        return WebsiteSettings::get('enabledModules') ?? [];
+        if (!isset(self::$cache[__METHOD__])) {
+            $packageJsonRoot = JsonUtils::readFromFile(FRAMELIX_APP_ROOT . "/package.json");
+            $currentMajorVersion = (int)explode(".", $packageJsonRoot['version'])[0];
+            $modules = WebsiteSettings::get('enabledModules') ?? [];
+            // verify if module folder exist
+            // prevents enabled modules generate error when have been deleted
+            foreach ($modules as $key => $module) {
+                $packageJsonFile = FRAMELIX_APP_ROOT . "/modules/$module/package.json";
+                if (!file_exists($packageJsonFile)) {
+                    unset($modules[$key]);
+                } else {
+                    // check if module is still supported for current pagemyself version
+                    $packageData = JsonUtils::readFromFile($packageJsonFile)['pagemyself'] ?? null;
+                    if ($packageData) {
+                        if ($currentMajorVersion < $packageData['minMajorVersion'] || $currentMajorVersion > $packageData['maxMajorVersion']) {
+                            unset($modules[$key]);
+                        }
+                    }
+                }
+            }
+            self::$cache[__METHOD__] = $modules;
+        }
+        return self::$cache[__METHOD__];
     }
 }
